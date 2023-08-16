3fe48c2edc5cec1fbf461981144b93c876724a35
epangar <e.pan.gar@gmail.com>
2023-08-16 10:52:45 +0200

    Ship class

---
+++ src/Classes/Ship-class.ts
@@ -0,0 +1,31 @@
+export class Ship{
+
+    distance: number;
+	distanceToCamp: number;
+	isBeingFollowed: boolean;
+    hasFoundTheCyborg: boolean;
+    hasFoundTheTown: boolean;
+    hasFoundTheStorm: boolean;
+    thirst: number;
+	shipHeat: number;
+	maxShipHeat: number;
+	water: number;
+	maxWater: number;
+
+	constructor(){
+
+		this.distance = 0;
+		this.distanceToCamp = 200;
+		this.isBeingFollowed = false;
+		this.hasFoundTheCyborg = false;
+		this.hasFoundTheTown = false;
+		this.hasFoundTheStorm = false;
+		this.thirst = 0;
+		this.shipHeat = 0;
+		this.maxShipHeat = 8;
+		this.water = 3;
+		this.maxWater = 3;
+	}
+
+	
+}
