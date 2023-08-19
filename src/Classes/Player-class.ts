c681d4f16b9ea1882c65056cf481cd3dec87e428
epangar <e.pan.gar@gmail.com>
2023-08-20 00:49:19 +0200

    Player class

---
+++ src/Classes/Player-class.ts
@@ -0,0 +1,30 @@
+export class Player {
+    
+
+    
+    moderate():string{
+        return "moderate";
+    }
+
+    full():string{
+        return "full";
+    }
+
+    drink():string{
+        return "drink";
+    }
+
+    stop():string{
+        return "stop";
+    }
+
+    restart():string{
+        return "restart";
+    }
+
+    quit():string{
+        return "quit";
+    }
+
+    
+}
